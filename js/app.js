8460fcd4b63c99c88bd5cdd8bef717ec339a0b7f
suhrob <suxrobamirov00@gmail.com>
2024-12-09 01:12:16 +0500

    scdv

---
+++ js/app.js
@@ -0,0 +1,86 @@
+// /////////////////////////////////////////////  1 M  /////
+
+// let son1 = prompt("Soni kiriting", Number());
+// let son2 = son1 * 10;
+// let son3 = 0;
+// let son4 = 0;
+// let son5 = 0;
+
+// for (let i = 11; i < son2; i += 2) {
+//     son3 += i;
+// }
+
+// for (let j = 12; j < son2; j+= 2) {
+//     son4 += j;
+// }
+
+// son5 = (son3 - son4)/10;
+// console.log("Javobi: ", son5);
+
+
+
+// ///////  2-yo'l  ///////
+
+
+// let son1 = parseInt(prompt("soni kiriting: "));
+// let son2 = son1*(-0.5)+son1+0.5;
+// console.log("Javob: ",son2);
+
+
+
+// ////////////////////////////////////////////  2 M  /////
+
+// let son1 = parseInt(prompt("soni kiriting: "));
+// let son2 = 0;
+
+// for (let i = 1; i <= son1; i++) {
+//     let son3 = 1
+//     for (let j = 1; j <= i; j++) {
+//         son3 *=j
+//     }
+//     console.log(son3);
+//     son2 += son3;
+// }
+
+// console.log(son2);
+
+
+
+// ////////////////////////////////////////////  3 M  /////
+
+// let son1 = parseInt(prompt("soni kiriting: "));
+// let son2 = son1 / 2;
+// let son3;
+
+// for (let i = 1; i <= son2; i++) {
+//     if ( i*i <= son1){
+//         son3 = i;
+//     }
+// }
+
+// console.log("javabi: ", son3);
+
+
+
+// ////////////////////////////////////////////  4 M  /////
+
+// let son1 = parseInt(prompt("soni kiriting: "));
+
+// for (let i = 1; i <= son1; i++) {
+//     if ( i*i > son1){
+//         console.log("javabi: ", i);
+//         break;
+//     }
+// }
+
+
+
+////////////////////////////////////////////  5 M  /////
+
+// let son1 = parseInt(prompt("soni kiriting: "));
+
+// if (son1 % 2 == 0) {
+//     console.log(son1/2);
+// }else{
+//     console.log((son1-1)/2+1);
+// }
